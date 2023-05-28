0620fecefcaaeee8d01140e82727913a6b156325
zox47 <abdellahmoutassadeq392@gmail.com>
2023-05-28 17:40:16 +0100

    add colors terminal

--- bot-amazon.py
+++ bot-amazon.py
@@ -1,174 +1,3 @@
-
-
-##import requests
-##import os
-##import json
-##from fake_useragent import UserAgent
-##pyload = {"email":"abdellahmoutassad.eq392@gmail.com"}
-##
-##ua = UserAgent()
-##
-##
-###title = input("Enter keyword you want: ")
-##
-##
-##useragent = ua.random
-##s = requests.Session()
-##headers = {
-## "Content-Type": "application/json",
-## "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-##}
-##
-##response = requests.post("https://lexica.art/api/pre-signup", json=json.dumps(pyload), headers=headers)
-##
-###change = s.get("https://www.amazon.com/portal-migration/hz/glow/get-location-label?storeContext=books&pageType=Search&actionSource=desktop-modal", headers=headers)
-##
-##
-##print (response.text)
-
-
-
-##
-##
-##import requests
-##import os
-##import json
-##from fake_useragent import UserAgent
-##from bs4 import BeautifulSoup
-##
-##ua = UserAgent()
-##useragent = ua.random
-##
-##pyload = {
-##  "locationType": "LOCATION_INPUT",
-##  "zipCode": "10001",
-##  "storeContext": "generic",
-##  "deviceType": "web",
-##  "pageType": "Gateway",
-##  "actionSource": "glow",
-##  
-##}
-##
-##ua = UserAgent()
-##
-##
-###title = input("Enter keyword you want: ")
-##
-##
-##useragent = ua.random
-##s = requests.Session()
-##headers = {
-## "Content-Type": "application/json",
-## "User-Agent": useragent}
-##
-##premier = s.get('https://www.amazon.com/', headers=headers)
-##
-##from bs4 import BeautifulSoup
-##soup = BeautifulSoup(premier.text, 'html.parser')
-##span = soup.find("span", id="nav-global-location-data-modal-action")
-##
-##attr = span["data-a-modal"]
-##
-##json_data = json.loads(attr)
-##
-### Extract the value of the "anti-csrftoken-a2z" key
-##anti_csrftoken_a2z = json_data['ajaxHeaders']['anti-csrftoken-a2z']
-##
-###print(anti_csrftoken_a2z)
-##
-##
-##
-##headerss = {
-## "Content-Type": "application/json",
-## "User-Agent": ,
-## "Accept": "text/html,*/*",
-## "Accept-Encoding": "gzip, deflate, br",
-## "Accept-Language": "en-US,en;q=0.9",
-## "Anti-Csrftoken-A2z": anti_csrftoken_a2z,
-## "Content-Length": "138",
-## "Device-Memory": "8",
-## "Downlink": "3.2",
-## "Dpr": "1.25",
-## "Ect": "3g",
-## "Origin": "https://www.amazon.com",
-## "Referer": "https://www.amazon.com/",
-## "Rtt": "250",
-## "Sec-Ch-Device-Memory": "8",
-## "Sec-Ch-Dpr": "1.25",
-## "Sec-Ch-Ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-## "Sec-Ch-Ua-Mobile": "?0",
-## "Sec-Ch-Ua-Platform": "Windows",
-## "Sec-Ch-Ua-Platform-Version": "10.0.0",
-## "Sec-Ch-Viewport-Width": "730",
-## "Sec-Fetch-Dest": "empty",
-## "Sec-Fetch-Mode": "cors",
-## "Sec-Fetch-Site": "same-origin",
-##
-##}
-##response = s.post("https://www.amazon.com/portal-migration/hz/glow/address-change?actionSource=glow", json=pyload, headers=headerss)
-##responses = s.get("https://www.amazon.com", headers=headerss)
-##print (response.text)
-##
-##
-##soups = BeautifulSoup(responses.text, 'html.parser')
-##spanx = soup.find("span", id="nav-global-location-data-modal-action")
-##print (spanx)
-##
-###print (response.text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##from selenium import webdriver
-##from webdriver_manager.chrome import ChromeDriverManager
-##from selenium.webdriver.chrome.options import Options
-##import pause
-##
-##options = Options()
-##options.add_experimental_option('detach', True)
-##driver = webdriver.Chrome(options=options)
-##
-##
-##driver.get("https://www.amazon.com/")
-##driver.find_element("id", "nav-global-location-popover-link").click()
-##pause.seconds(3)
-##driver.find_element("id", "GLUXZipUpdateInput").send_keys(10001)
-##pause.seconds(3)
-##
-##driver.find_element("name", "GLUXZipUpdate").click()
-##
-##driver.find_element("name", "glowDoneButton").click()
-##pause.seconds(3)
-##driver.find_element("name", "searchDropdownBox").click()
-##pause.seconds(3)
-##driver.find_element("xpath", '//*[@id="searchDropdownBox"]/option[6]').click()
-##
-
-
-<<<<<<< HEAD
-
-=======
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -190,23 +19,19 @@ print (f"""{bcolors.HEADER}
 {bcolors.ENDC}
 {bcolors.BOLD}BY MR.ZOX47{bcolors.ENDC}
 """)
->>>>>>> 13971e0 (This is a new commit for what I originally planned to be amended)
         
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pause
+#import pause
 from selenium.webdriver.support.ui import Select
 import csv
 import datetime
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 13971e0 (This is a new commit for what I originally planned to be amended)
 # Get the current date and time
 current_datetime = datetime.datetime.now()
 
@@ -339,20 +164,12 @@ driver.execute_script("window.open();")
 # Switch to the new tab
 driver.switch_to.window(driver.window_handles[1])
 
-<<<<<<< HEAD
-mainkeyword = 'fairy coloring book for adult'
-=======
 mainkeyword = input(f"{bcolors.OKGREEN}+{bcolors.ENDC} {bcolors.BOLD}Enter The Main Keywword: {bcolors.ENDC}")
->>>>>>> 13971e0 (This is a new commit for what I originally planned to be amended)
 query = ('chrome-extension://hbapdpeemoojbophdfndmlgdhppljgmp/html/page.html?page=autocomplete&query={0}&service=amazon').format(mainkeyword)
 driver.get(query)
 
 listk = []
-<<<<<<< HEAD
-input('are the keywords downloaded!: ')
-=======
 input(f'{bcolors.OKGREEN}+{bcolors.ENDC} {bcolors.BOLD}Are the keywords downloaded! | Press Enter{bcolors.ENDC} \n')
->>>>>>> 13971e0 (This is a new commit for what I originally planned to be amended)
 
 til = mainkeyword.replace(' ', '-')
 csv_file_path = (r'amazon-keywords-{0}--{1}.csv').format(til, formatted_datetime)
@@ -376,8 +193,9 @@ with open(csv_file_path, 'r') as file:
 
 driver.close()
 driver.switch_to.window(driver.window_handles[0])
-
+count = 0
 for i in listk:
+    count += 1
     try:
         driver.find_element("id", 'twotabsearchtextbox').clear()
     except:
@@ -385,18 +203,15 @@ for i in listk:
     driver.find_element("id", 'twotabsearchtextbox').send_keys(i)
     driver.find_element("id", 'nav-search-submit-button').click()
     res = driver.find_element(By.XPATH, '//*[@id="search"]/span/div/h1/div/div[1]/div/div/span[1]').text
-    try:
+    if "over" in str(res):
         results = res.partition('over ')[2]
-    except:
+    elif "of" in str(res):
         results = res.partition('of ')[2]
+    else:
+        results = res
     ress = results.partition(' results for')[0]  
-<<<<<<< HEAD
-    print (i+'------> '+ress)
-=======
-    print (i+f'{bcolors.OKGREEN}--------->{bcolors.ENDC}'+ress)
->>>>>>> 13971e0 (This is a new commit for what I originally planned to be amended)
-
-
+   # print (i+'------> '+ress)
+    print (f"{bcolors.OKGREEN}[{count}]{bcolors.ENDC} "+i+f'{bcolors.OKGREEN} |---------> {bcolors.ENDC}'+ress+"\n")
 
 
 
